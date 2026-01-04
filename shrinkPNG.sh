945dc0ddf96a32b47690635997f248f9059127db
Bryan Opfer <824368+bopfer@users.noreply.github.com>
2026-01-04 12:30:16 -0800

    Use oxipng which is much faster

--- shrinkPNG.sh
+++ shrinkPNG.sh
@@ -5,11 +5,15 @@
 #
 # Dependencies:
 #   brew install pngquant
+#   brew install oxipng
 #   brew install zopfli
 #
 # Usage:
 #   ./shrinkPNG.sh <filename>.png
 ##
 
-pngquant 64 --skip-if-larger --strip --ext=.png --force "$1"
-zopflipng -y "$1" "$1"
+pngquant 256 --skip-if-larger --strip --ext=.png --force "$1"
+oxipng -o 4 -s "$1"
+
+# Very slow, minimal additional savings
+# zopflipng -y "$1" "$1"
